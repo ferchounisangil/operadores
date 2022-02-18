5bcaab9f8e4873aff192487a1963a71a7fbc31f3
Fernando Rodriguez <luidrodriguez1@gmail.com>
2022-02-17 19:34:31 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let number1 = parseInt(prompt('Digite un numero',1));
+let number2 = parseInt(prompt('Digite un numero',1));
+let resultado;
+console.log( `Valores para operaciones ${number1} &&  ${number2}` );
+
+resultado = (number1+number2);
+console.log('Suma ' + resultado)
+
+resultado = number1-number2;
+console.log('Resta ' + resultado)
+
+resultado = number1*number2;
+console.log('Multiplicación ' + resultado)
